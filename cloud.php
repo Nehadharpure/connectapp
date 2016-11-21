fe21309c82e0069af07dcf5c18e772123529d004
Nehadharpure <usha.dharpure@technorealistic.com>
2016-11-21 14:42:24 +0530

    Update cloud.php

--- cloud.php
+++ cloud.php
@@ -11,7 +11,7 @@
 
 //error_reporting(0);
 //$servernameC = "10.140.0.2";
-$servernameC = "104.198.41.250";
+$servernameC = "27.58.63.18";
 $usernameC = "root";
 $passwordC = "root@123";
 $dbnameC = "inventory";
