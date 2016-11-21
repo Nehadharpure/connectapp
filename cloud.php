a065dc136a560f97a6d44f294d3da7ad0df6365c
Nehadharpure <usha.dharpure@technorealistic.com>
2016-11-21 19:06:56 +0530

    Update cloud.php

--- cloud.php
+++ cloud.php
@@ -11,13 +11,36 @@
 $dsn = getenv('MYSQL_DSN');
 $user = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
+  
+$connC = new PDO($dsn, $user, $password);
+$connC->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+function get_user()
+    {
+		global $connC;
+		$sql = "Select * from dim_user";
+		$result = $connC->query($sql);
+		return $result;
+    }
+	?>
+<h1> Application connectivity with cloud sql </h1>
 
-try {
-    $dbh = new PDO($dsn, $user, $password);
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
-
-?>
+<table width="200" border="1">
+  <tr>
+    <th>Sr</th>
+    <th>User ID</th>
+    <th>User Type</th>
+  </tr>
+  <?php 
+  $getuser = get_user();
+  $i=1;
+  while($showuser = $getuser->fetch())
+  {?>
+<tr>
+    <td><?php echo $i++;?></td>
+    <td>&nbsp;<?php echo $showuser['User_ID'];?></td>
+    <td>&nbsp;<?php echo $showuser['Users_Type'];?></td>
+  </tr>
+ <?php } ?>
+</table>
 </body>
 </html>
